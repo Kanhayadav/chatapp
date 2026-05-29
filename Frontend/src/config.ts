192f2fe14f2459cf11757ede0b834e496ccefecf
Kanhayadav <kanhayadav1610@gmail.com>
2026-05-29 18:17:11 +0530

    problme in the config file fixed

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,5 +1,5 @@
 
-export const DBkey = import.meta.env.dataBaseKey as string
+export const DBkey = import.meta.env.VITE_DATABASE_KEY as string
 export const Backend = import.meta.env.VITE_BACKEND_KEY as string
 
 
